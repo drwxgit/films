b9c13fa5bf65a3f4b3d755e64f8851d167ef4648
drwxgit <d_rwx@yahoo.it>
2015-08-26 18:06:28 +0200

    Added truncation of string

--- films.py
+++ films.py
@@ -8,14 +8,17 @@ DATABASE = 'films'
 '''QUERY'''
 SELECT_ALL = 'SELECT * FROM film'
 
+def truncateIfNecessary(iStr, iMax):
+    return (iStr[:iMax]+'...') if len(iStr) > iMax else iStr
 
 def doSelectAll(cur):
     rowCount = cur.execute(SELECT_ALL)
     
     if (rowCount > 0):
         print('%-5s\t%-20s\t%-20s\t%-20s\t%-4s\t%-6s' % ('IDX', 'TITOLO', 'ATTORI', 'REGISTA', 'ANNO', 'GENERE'))
+
         for r in cur.fetchall():
-            print('%-5s\t%-20s\t%-20s\t%-20s\t%-4s\t%-6s' % (r[0],(r[1][:20]+'...') if len(r[1]) > 20 else r[1],r[2],r[3],r[4],r[5]))
+            print('%-5s\t%-20s\t%-20s\t%-20s\t%-4s\t%-6s' % (r[0], truncateIfNecessary(r[1],20), truncateIfNecessary(r[2],20), truncateIfNecessary(r[3],20), r[4], r[5]))
     else:
         print('EMPTY')
 
